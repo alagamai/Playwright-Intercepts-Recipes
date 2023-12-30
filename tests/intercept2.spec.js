85ed6e7dd8bf8f2c743a4d40b03862011323dabc
Alagamai <alagamai.n@gmail.com>
2023-12-30 13:16:42 +0530

    Update intercept2.spec.js

--- tests/intercept2.spec.js
+++ tests/intercept2.spec.js
@@ -1,17 +1,18 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect, chromium } = require('@playwright/test');
 const usersData = require('./users.json');
 
-test('Intercept by Url', async ({ page }) => {
+test('Delete Headers', async ({ page }) => {
 	await page.goto('https://reqres.in/');
 
 	// Intercept network requests
 	await page.route('**/api/users/', route => {
 		const headers = route.request().headers();
-		console.log(`Headers: ${JSON.stringify(headers)}`);
+		console.log(`Headers Before Delete: ${JSON.stringify(headers, null, 2)}`);
 		//Delete the 'X-Secret' header if it exists
-		if ('cookie' in headers) {
-			delete headers['cookie'];
+		if ('origin' in headers) {
+			delete headers['origin'];
+			console.log(`Headers after Delete: ${JSON.stringify(headers, null, 2)}`);
 		}
 
 		route.continue();
@@ -23,6 +24,43 @@ test('Intercept by Url', async ({ page }) => {
 	const responseBody = await response.json();
 
 	expect(responseBody.data).toHaveLength(6);
+});
+
+test('loads page without images', async ({ page }) => {
+	// Block url that ends with png or jpeg images.
+	await page.route(/(png|jpeg|img|image|jpg)$/, route => route.abort());
+
+	await page.goto('https://demoblaze.com/');
+	// ... test goes here
+});
+
+test('modify request at context level', async () => {
+	const browser = await chromium.launch();
+	const context = await browser.newContext();
+
+	await context.route('**/*', route => {
+		//if (route.request().resourceType() === 'xhr') {
+		const orginalHeaders = route.request().headers();
+		orginalHeaders['user-agent'] = 'Custom User Agent';
+		// Log the original and modified headers
+		// console.log('Original Headers:', orginalHeaders);
+		console.log('Original Headers:', route.request().headers());
+
+		route.continue({ headers: orginalHeaders });
+		console.log('Modified Headers:', route.request().headers());
+		//} else {
+		// Continue other requests without modification
+		//	route.continue();
+		//}
+	});
+
+	const page = await context.newPage();
+	await page.goto('https://example.com');
+
+	const page1 = await context.newPage();
+	await page1.goto('https://demoblaze.com/');
+
+	await browser.close();
 });
 
 // test('Read headers', async ({ page }) => {
